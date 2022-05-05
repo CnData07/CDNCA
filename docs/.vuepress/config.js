fdc0595c68fd3bb943ca383dee0b4f816c7e7e34
CnData07 <data07@126.com>
2022-05-06 00:21:20 +0800

    new

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    base:"/CDNCA/",
+    title: 'CDNCA',
+    description: '党的十八大，中国特色社会主义进入新时代',
+    themeConfig: {
+        logo: '/assets/img/logo.jpg',
+        navbar: true,
+        sidebar: [
+            '/'
+        ],
+
+        lastUpdated: '最后更新时间',
+    }
+}
